3f09fba3152b2010fc10b81df3fa7353c9a20a6a
Linas Stonkus <liniusst@gmail.com>
2023-02-27 21:43:03 +0200

    add exercise 3

--- main.py
+++ main.py
@@ -3,6 +3,23 @@
 
 # source .venv/bin/activate
 
-import numpy as np
+# Use any of virtual environment option , install random-word package and create shot python script 
+# which would print out 5 random word (names must be all capital letter and sorted.)
+import logging
+logging.basicConfig(level=logging.DEBUG,filename='data.log', filemode='w', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
+from random_word import RandomWords
+from typing import List
 
-print(np.sqrt(9))
+def get_name_surname() -> List[str]:
+    values = 0
+    while values < 5:
+        word = RandomWords()
+        name = word.get_random_word()
+        surname = word.get_random_word()
+        data = sorted([name.capitalize(), surname.capitalize()])
+        logging.info(f'Random name and surname: "{data[0]} {data[1]}" - sorted by alphabetic')
+        values += 1
+    return data
+
+if __name__ == "__main__":
+    get_name_surname()
